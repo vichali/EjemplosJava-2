6550dfe56d1c1fbb873ea4e808a117555e0dbad0
Victor <victorhugo.chalian@gmail.com>
2012-08-13 19:14:46 -0300

    modificado
    Signed-off-by: Victor <victorhugo.chalian@gmail.com>

--- EjemplosJava-2/src/creacionDeArreglos/VectoresUso_5.java
+++ EjemplosJava-2/src/creacionDeArreglos/VectoresUso_5.java
@@ -5,8 +5,8 @@ public class VectoresUso_5 {
 	// uso mejorado del for en vectores
 	public void imprimirElementos(String[] dias) {
 		
-			for (String diasSemana : dias) {
-			System.out.println(diasSemana);
+			for (String mostrarDias : dias) {
+			System.out.println(mostrarDias);
 		}
 	}
 	
